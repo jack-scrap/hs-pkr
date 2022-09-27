fe8515cfea315ab99db5fc0bf08368911bb3bedb
Jack <jackhasakeyboard@gmail.com>
2022-09-26 20:59:07 -0600

    Updated colors

--- res/shad/solid.fs
+++ res/shad/solid.fs
@@ -1,5 +1,15 @@
 #version 400
 
+vec3[2] grey = vec3[2](
+	vec3(34, 34, 34),
+	vec3(102, 102, 102)
+);
+
+vec3[2] purple = vec3[2](
+	vec3(69, 59, 87),
+	vec3(97, 82, 134)
+);
+
 void main() {
-	gl_FragColor = vec4(vec3(1.0), 1.0);
+	gl_FragColor = vec4(purple[int(false)] / 255.0, 1.0);
 }
