d0ccfa46688e5037276643698e74e63c004dcfab
Jack <jackhasakeyboard@gmail.com>
2022-09-27 19:20:47 -0600

    Drew hand of cards

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <GL/glew.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -17,7 +18,10 @@ extern const unsigned int res[2] = {
 int main() {
 	Disp disp("Poker", res[X], res[Y]);
 	
-	Card card(false, glm::vec2(0.0, 5.0));
+	std::vector<Card*> hand;
+	for (int i = 0; i < 5; i++) {
+		hand.push_back(new Card(false, glm::vec2(i * 2.0, 5.0)));
+	}
 
 	SDL_Event e;
 	while (disp.open) {
@@ -29,7 +33,9 @@ int main() {
 
 		disp.clear(0, 0, 0, 1);
 
-		card.draw();
+		for (Card* card : hand) {
+			card->draw();
+		}
 
 		disp.update();
 	}
